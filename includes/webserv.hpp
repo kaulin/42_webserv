a4d0d2d9bbeac05d58566ff030ccbb742fed5495
Katja Toivola <ktoivola@student.hive.fi>
2024-12-03 16:24:12 +0200

    first addition. Adding function parse_config_file and adding some temp data. Added server.cpp file for main server logic

--- includes/webserv.hpp
+++ includes/webserv.hpp
@@ -2,5 +2,13 @@
 #define WEBSERV_HPP
 
 # include <iostream>
+# include <sys/socket.h>
+# include <sys/types.h>
+# include <netdb.h>
+# include <netinet/in.h>
+# include <arpa/inet.h>
+
+void    parse_config_file(const std::string conf, struct addrinfo serv_addr);
+void	run_server(struct addrinfo serv_addr);
 
 #endif
---
+++ sources/server.cpp
@@ -0,0 +1,11 @@
+#include "webserv.hpp"
+
+void    setup_server()
+{
+
+}
+
+void    run_server(struct addrinfo serv_addr)
+{
+    setup_server();
+}
