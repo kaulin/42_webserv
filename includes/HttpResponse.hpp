0e82e9fb90d41c0f4dc07060d0b160d93e60960c
Jaakko Junttila <jajuntti@student.hive.fi>
2024-12-09 15:23:00 +0200

    Adds some container structuring to response class to handle headers in the correct order

--- includes/HttpResponse.hpp
+++ includes/HttpResponse.hpp
@@ -1,16 +1,30 @@
-// Response.hpp
+// HttpResponse.hpp
 #pragma once
 
+#include <iostream>
 #include <string>
+#include <deque>
+#include <unordered_map>
 
-class Response
+// Temp placeholders
+#define START_LINE "HTTP/1.1 200 OK\n"
+#define BODY "<!DOCTYPE html><html><head><title>index.html</title></head><body><h1>Hello World!</h1><body></html>"
+
+class HttpResponse
 {
 private:
-	std::string _body;
 	int _statusCode;
+	std::deque<std::string> _headerKeys;
+	std::unordered_map<std::string, std::string> _headers;
+	std::string _body;
 public:
-	Response(/* args */);
-	~Response();
+	HttpResponse();
+	~HttpResponse(); 
+	void setStatusCode(const int statusCode);
+	void addHeader(const std::string& key, const std::string& value);
+	void setBody(const std::string& body);
+	const std::string HttpResponse::buildResponse() const;
+	friend std::ostream& std::operator<<(std::ostream& os, const HttpResponse& obj);
 };
 
 /*
