b1d3ee8b84dd3a262c93438b76727bb027d3b623
Jaakko Junttila <jajuntti@student.hive.fi>
2024-12-20 16:04:29 +0200

    Constructs a complete response from a request. Currently defaults to generated 501 Not Implemented page, regardless of request.

--- sources/HttpResponse.cpp
+++ sources/HttpResponse.cpp
@@ -1,16 +1,27 @@
 #include "HttpResponse.hpp"
 
+static std::string timeStamp()
+{
+	auto t = std::time(nullptr);
+	auto tm = *std::localtime(&t);
+	std::ostringstream oss;
+	oss << std::put_time(&tm, "%a, %d %b %Y %H:%M:%S %Z");
+	return oss.str();
+}
+
 // Constructor
 HttpResponse::HttpResponse(HttpRequest& request) : 
 	_request(request),
 	_resolved(false), 
 	_statusCode(0)
 {
-	// checkRequest(); // check request errors (eg POST with no type or transfer encoding)
-	// checkMethod(); // check method & allowed methods at location
-	// checkResource(); // check if resource exists
-	// checkCGI; // check cgi, execute cgi
-	// handleMethod; // handle method
+	checkRequest(); // check request errors (eg POST with no type or transfer encoding)
+	if (!_resolved) checkMethod(); // check method & allowed methods at location
+	if (!_resolved) checkResource(); // check if resource exists
+	if (!_resolved) checkCGI; // check cgi, execute cgi
+	if (!_resolved) handleMethod; // handle method
+	if (!_resolved) _statusCode = STATUS_INTERNAL_ERROR;
+	formResponse();
 }
 
 // Deconstructor
@@ -23,34 +34,77 @@ void HttpResponse::addHeader(const std::string& key, const std::string& value)
 	_headers.insert(key, value);
 }
 
-// Setter of std::string _body
-void HttpResponse::setBody(const std::string& body) { _body = body; }
+void HttpResponse::checkRequest()
+{
+	_statusCode = STATUS_NOT_IMPLEMENTED;
+	_resolved = true;
+}
 
-const std::string HttpResponse::getStatusText() const {
+void HttpResponse::checkMethod()
+{
+
+}
+
+void HttpResponse::checkResource()
+{
+	
+}
+
+void HttpResponse::checkCGI()
+{
+	
+}
+
+void HttpResponse::handleMethod()
+{
+	
+}
+
+void HttpResponse::formResponse()
+{
+	std::string status = getStatus();
+
+	_statusLine = _request.http_version + " " + status + "\n";
+	if (_statusCode >= 300)
+	{ 
+		_body = 
+			"<html><head><title>" +
+			status +
+			"</title></head><body><center><h1>" +
+			status +
+			"</h1></center><hr><center>nginx/1.27.3</center></body></html>";
+	}
+	addHeader("Server", "Webserv v0.6.6.6");
+	addHeader("Content-Length", std::to_string(_body.size()));
+	addHeader("Content-Type", "text/html");
+	addHeader("Connection", "Closed");
+}
+
+const std::string HttpResponse::getStatus() const {
 	switch (_statusCode)
 	{
 		case 200:
-			return ("OK");
+			return ("200 OK");
 		case 201:
-			return ("Created");
+			return ("201 Created");
 		case 204:
-			return ("No Content");
+			return ("204 No Content");
 		case 400:
-			return ("Bad Request");
+			return ("400 Bad Request");
 		case 403:
-			return ("Forbidden");
+			return ("403 Forbidden");
 		case 404:
-			return ("Not Found");
+			return ("404 Not Found");
 		case 405:
-			return ("Method Not Allowed");
+			return ("405 Method Not Allowed");
 		case 413:
-			return ("Content Too Large");
+			return ("413 Content Too Large");
 		case 414:
-			return ("URI Too Long");
+			return ("414 URI Too Long");
 		case 500:
-			return ("Internal Server Error");
+			return ("500 Internal Server Error");
 		default :
-			return ("Not Implemented");
+			return ("501 Not Implemented");
 
 	}
 }
@@ -60,7 +114,8 @@ const std::string HttpResponse::toString() const
 {
 	std::string response;
 	
-	response += START_LINE;
+	response += _statusLine;
+	response += "Date: " + timeStamp() + "\n";
 	for (std::string key : _headerKeys)
 	{
 		response += key + ": " + _headers.at(key) + "\n";
