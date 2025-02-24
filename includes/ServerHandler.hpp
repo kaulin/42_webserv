6d4d5cbbfebd183d5db1075f0c076a53a6b02a7d
Jaakko Junttila <jajuntti@student.hive.fi>
2025-02-24 16:29:24 +0200

    Fixes previous rebase issues

--- includes/ServerHandler.hpp
+++ includes/ServerHandler.hpp
@@ -5,43 +5,46 @@
 class HttpServer;
 
 typedef struct s_client {
-	int	fd;
-	std::string requestString;
-	bool requestReady;
-	HttpRequest* request;
-	std::string responseString;
-	int responseCode;
-	bool responseReady;
-	std::time_t lastRequest;
-	bool keep_alive;
+	int				fd;
+	std::string		requestString;
+	bool			requestReady;
+	HttpRequest*	request;
+	std::string		responseString;
+	int				responseCode;
+	bool			responseReady;
+	std::time_t		lastRequest;
+	bool			keep_alive;
 } t_client;
 
 class ServerHandler
 {
 private:
 	std::vector<std::shared_ptr<HttpServer>>	_servers;
-	size_t					  _serverCount;
-	std::vector<int>			_ports;
-	std::unordered_map<int, t_client> _clients;
-	std::vector<struct pollfd>  _pollFds;
-	bool						 _running;
+	size_t					 					_serverCount;
+	std::vector<int>							_ports;
+	std::unordered_map<int, t_client>			_clients;
+	std::vector<struct pollfd>					_pollFds;
+	bool										_running;
+	ServerConfigData							_config;
 public:
-	ServerHandler();
+	ServerHandler(std::string path);
 	~ServerHandler();
 
-	void	runServers();
-	void	setupSockets();
-	void	setupServers(std::string path);
-	void	pollLoop();
-	void	setPollList();
-	void	error_and_exit(const char *msg);
-	void	addConnection(size_t& i);
-	void	closeConnection(size_t& i);
-	void	readRequest(size_t& i);
-	void	sendResponse(size_t& i);
-	void	cleanupServers();
-	size_t	getPortCount();
+	void		runServers();
+	void		setupSockets();
+	void		setupServers();
+	void		pollLoop();
+	void		setPollList();
+	void		error_and_exit(const char *msg);
+	void		addConnection(size_t& i);
+	void		closeConnection(size_t& i);
+	void		readRequest(size_t& i);
+	void		sendResponse(size_t& i);
+	void		cleanupServers();
+	size_t		getPortCount();
 	static void	signalHandler(int);
+
+	void	printPollFds();
 
 	// Helper functions for debugging
 	// void	*get_in_addr(struct sockaddr *sa);
--- sources/ServerHandler.cpp
+++ sources/ServerHandler.cpp
@@ -6,14 +6,14 @@
 
 #define BACKLOG 10 // how many pending connections queue will hold
 
-ServerHandler::ServerHandler() {
-	
-	_servers.clear();
-	_pollFds.clear();
-	_ports.clear();
-	_serverCount = 0;
+ServerHandler::ServerHandler(std::string path) : 
+	_config(ServerConfigData(path))
+{
+	_serverCount = _config.getServerCount();
+	_servers.reserve(_serverCount);
+	_ports.reserve(_config.getPortCount());
 	_running = false;
-	std::cout << "Constructor Size of pollfd list: " << _pollfd_list.size() << "\n";
+	std::cout << "Constructor Size of pollfd list: " << _pollFds.size() << "\n";
 }
 
 ServerHandler::~ServerHandler() 
@@ -68,7 +68,7 @@ void	ServerHandler::sendResponse(size_t& i)
 	the constructor and the appropriate values in the class instance is set. 
 	Going through all serverBlock instances, a new shared pointer is made 
 	for each virtual server with the appropriate configurations */
-void	ServerHandler::setupServers(std::string path)
+void	ServerHandler::setupServers()
 {
 	for (const auto& [servName, config] : _config.getConfigBlocks()) 
 	{
@@ -78,7 +78,7 @@ void	ServerHandler::setupServers(std::string path)
 	{
 		server->setupAddrinfo();
 	}
-	_server_count = _servers.size();
+	_serverCount = _servers.size();
 
 	// HttpServer  serverInstance(current);
 	// serverInstance.setPorts(current.getPorts());
@@ -86,25 +86,29 @@ void	ServerHandler::setupServers(std::string path)
 	// serverInstance.setupAddrinfo();
 	// _servers.push_back(serverInstance);
 }
-
 void	ServerHandler::addConnection(size_t& i) {
 	int clientFd;
 	socklen_t addrlen;
 	struct sockaddr_storage remoteaddr_in;
 	struct pollfd new_pollfd;
 
-	addrlen = sizeof(remoteaddr_in);
-	clientFd = accept(_pollFds[i].fd, (struct sockaddr *)&remoteaddr_in, &addrlen);
-	if (clientFd == -1) 
-	{
-		error_and_exit("Accept"); // log error
+	try {
+		addrlen = sizeof(remoteaddr_in);
+		clientFd = accept(_pollFds[i].fd, (struct sockaddr *)&remoteaddr_in, &addrlen);
+		if (clientFd == -1)
+			throw std::runtime_error("Internal Server Error 500: accept failed");
+		if (fcntl(clientFd, F_SETFL, O_NONBLOCK))
+			throw std::runtime_error("Internal Server Error 500: fcntl failed");
+		new_pollfd.fd = clientFd;
+		new_pollfd.events = POLLIN | POLLOUT;
+		new_pollfd.revents = 0;
+		_pollFds.emplace_back(new_pollfd);
+		t_client client = {};
+		_clients[clientFd] = client;
+	} catch (std::exception& e) {
+		_clients[clientFd].responseCode = 500;
+		_clients[clientFd].responseReady = true;
 	}
-	new_pollfd.fd = clientFd;
-	new_pollfd.events = POLLIN | POLLOUT;
-	new_pollfd.revents = 0;
-	_pollFds.emplace_back(new_pollfd);
-	t_client client = {};
-	_clients[clientFd] = client;
 }
 
 void	ServerHandler::closeConnection(size_t& i) {
@@ -121,20 +125,28 @@ void	ServerHandler::closeConnection(size_t& i) {
 void	ServerHandler::readRequest(size_t& i)
 {
 	int clientFd = _pollFds[i].fd;
-	int		numbytes;
-	char	buf[1024];
+	int receivedBytes;
+	char buf[1024] = {};
 
-	std::memset(buf, 0, 1024);
-	if ((numbytes = recv(clientFd, buf, 1023, 0)) == -1) 
-	{
-		if (errno == EAGAIN || errno == EWOULDBLOCK) {
-			std::cout << "No data available to read, try again later." << std::endl;
-		} else {
-			perror("recv");
-			error_and_exit("Receive");
+	try {
+		receivedBytes = recv(clientFd, buf, 1023, 0);
+		if (receivedBytes < 0)
+			throw std::runtime_error("Internal Server Error 500: recv failed");
+		else if (receivedBytes == 0)
+			closeConnection(i);
+		else {
+			_clients[clientFd].requestString.append(buf, receivedBytes);
+			if (receivedBytes < 1024) {
+				_clients[clientFd].requestReady = true;
+				std::cout << "Client [" << clientFd << "] request ready: " << _clients[clientFd].requestString << "\n";
+			}
+			else
+				std::cout << "Received request portion: " << buf << "\n";
 		}
+	} catch (std::exception &e) {
+		closeConnection(i);
+		throw ;
 	}
-	std::cout << "Received request: " << buf << "\n";
 }
 
 void	ServerHandler::setPollList()
@@ -165,19 +177,23 @@ void	ServerHandler::pollLoop()
 	setPollList();
 	while (_running)
 	{
-		if ((poll_count = poll(_pollFds.data(), _pollFds.size(), -1)) == -1) {
-			error_and_exit("Poll failed");
-		}
-		for(size_t i = 0; i < _pollFds.size(); i++)
-		{
-			if (_pollFds[i].revents & POLLIN) {
-				if (_clients.find(_pollFds[i].fd) == _clients.end())
-					addConnection(i);
-				else
-					readRequest(i);
+		try {
+			if ((poll_count = poll(_pollFds.data(), _pollFds.size(), -1)) == -1) {
+				error_and_exit("Poll failed");
 			}
-			else if (_pollFds[i].revents & POLLOUT && _clients[_pollFds[i].fd].requestReady == true)
-				sendResponse(i);
+			for(size_t i = 0; i < _pollFds.size(); i++)
+			{
+				if (_pollFds[i].revents & POLLIN) {
+					if (_clients.find(_pollFds[i].fd) == _clients.end())
+						addConnection(i);
+					else
+						readRequest(i);
+				}
+				else if (_pollFds[i].revents & POLLOUT && _clients[_pollFds[i].fd].requestReady == true)
+					sendResponse(i);
+			}
+		} catch (std::exception& e) {
+// Errors should be handled lower down and rethrown to be caught here.
 		}
 	}
 }
@@ -238,4 +254,23 @@ void	ServerHandler::runServers()
 	setupSockets();
 	pollLoop();
 	cleanupServers();
+}
+
+void	ServerHandler::printPollFds()
+{
+	for (auto& poll_obj : _pollFds) 
+	{
+		std::cout << "Polling on fd: " << poll_obj.fd << "\n";
+	}
+}
+
+size_t	ServerHandler::getPortCount()
+{
+	size_t	num_of_ports = 0;
+
+	for (auto& server : _servers) {
+		num_of_ports += server->getNumOfPorts();
+	}
+	std::cout << "Number of ports total: " << num_of_ports << "\n";
+	return (num_of_ports);
 }
