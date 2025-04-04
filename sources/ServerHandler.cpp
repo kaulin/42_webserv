ef6978a5f4e8ea3e9453c1647e19726d3b2ab0a6
Jaakko Junttila <jajuntti@student.hive.fi>
2025-04-04 15:59:44 +0300

    Removes file size from client struct

--- sources/ServerHandler.cpp
+++ sources/ServerHandler.cpp
@@ -92,7 +92,6 @@ void ServerHandler::resetClient(Client& client) {
 	client.responseReady = false;
 	client.responseSent = false;
 	client.responseCode = 200;
-	client.fileSize = 0;
 	client.fileReadFd = -1;
 	client.fileTotalBytesRead = 0;
 	client.fileWriteFd = -1;
@@ -210,15 +209,13 @@ void	ServerHandler::readFromFd(size_t& i) {
 
 	bytesRead = read(client.fileReadFd, buf, BUFFER_SIZE);
 	if (bytesRead <= 0)
-		throw std::runtime_error("Internal Server Error 500: read failed");
+		throw ServerException(STATUS_INTERNAL_ERROR);
 	else {
 		client.resourceString.append(buf, bytesRead);
 		client.fileTotalBytesRead += bytesRead;
-		std::cout << "Total bytes read/file size: " << client.fileTotalBytesRead << "/" << client.fileSize << "\n";
+		std::cout << "Total bytes read: " << client.fileTotalBytesRead << "\n";
 		if (bytesRead < BUFFER_SIZE)
 		{
-			if (client.fileTotalBytesRead != client.fileSize)
-				throw std::runtime_error("Internal Server Error 500: read failed");
 			client.requestReady = true;
 			std::cout << "Client " << client.fd << " resource read complete:\n" << client.resourceString << "\n";
 			close(client.fileReadFd);
@@ -235,22 +232,16 @@ void	ServerHandler::readFromFd(size_t& i) {
 void	ServerHandler::writeToFd(size_t& i) {
 	Client& client = *_clients[_pollFds[i].fd].get();
 	const HttpRequest& request = client.requestHandler->getRequest();
-	int bytesWritten;
-	size_t leftToWrite = client.fileSize - client.fileTotalBytesWritten;
-	size_t bytesToWrite = leftToWrite > BUFFER_SIZE ? BUFFER_SIZE : leftToWrite;
-	char buf[BUFFER_SIZE] = {};
-	request.body.copy(buf, BUFFER_SIZE, client.fileTotalBytesWritten);
+	size_t bytesWritten;
+	size_t leftToWrite = request.body.size();
 
-	bytesWritten = write(client.fileWriteFd, buf, bytesToWrite);
+	bytesWritten = write(client.fileWriteFd, request.body.c_str(), leftToWrite);
 	if (bytesWritten <= 0)
-		throw std::runtime_error("Internal Server Error 500: write failed");
+		throw ServerException(STATUS_INTERNAL_ERROR);
 	else {
 		client.fileTotalBytesWritten += bytesWritten;
-		std::cout << "Total bytes written/file size: " << client.fileTotalBytesWritten << "/" << client.fileSize << "\n";
 		if (bytesWritten < BUFFER_SIZE)
 		{
-			if (client.fileTotalBytesWritten != client.fileSize)
-				throw std::runtime_error("Internal Server Error 500: write failed");
 			client.requestReady = true;
 			std::cout << "Client [" << client.fd << "] POST request resource saved to disk\n";
 			close(client.fileWriteFd);
