857ed43777d365e863d54063edada599a486d590
Jaakko Junttila <jajuntti@student.hive.fi>
2025-04-30 13:55:24 +0300

    Simplifies logger static class

--- sources/Logger.cpp
+++ sources/Logger.cpp
@@ -1,24 +1,20 @@
 #include "Logger.hpp"
 
-std::string Logger::getCurrentTime() const
+Logger::Logger() {}
+Logger::~Logger() {}
+
+void Logger::log(eStatus ok, const std::string& message)
+{
+	std::cout << (ok ? "" : "[ERROR] ") 
+		<< "[" << getCurrentTime() << "] " 
+		<< message << "\n";
+}
+
+std::string Logger::getCurrentTime()
 {
 	auto now = std::chrono::system_clock::now();
 	std::time_t nowTime = std::chrono::system_clock::to_time_t(now);
 	std::stringstream ss;
 	ss << std::put_time(std::localtime(&nowTime), "%Y-%m-%d %H:%M:%S");
 	return ss.str();
-}
-
-// constructor for a console logger
-Logger::Logger() {}
-
-// destructor
-Logger::~Logger() {}
-
-// overload << operator for stream manipulators (like std::endl)
-Logger& Logger::operator<<(std::ostream& (*func)(std::ostream&))
-{
-	if (func == static_cast<std::ostream& (*)(std::ostream&)>(std::endl))
-		_buffer << "\n";
-	return *this;
 }
