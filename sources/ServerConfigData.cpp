0c3197c85c4c73d17117510ecf20c4fc21233279
Jaakko Junttila <jajuntti@student.hive.fi>
2025-05-09 13:11:29 +0300

    Adds getParentLocation to get location of parent dir

--- sources/ServerConfigData.cpp
+++ sources/ServerConfigData.cpp
@@ -37,6 +37,21 @@ size_t  ServerConfigData::getServerCount()
 const Location* ServerConfigData::getLocation(const Config& config, std::string path) {
 	if (path.size() > 1 && path.back() == '/')
 			path.erase(path.begin() + path.find_last_of('/'), path.end());
+	std::cout << "Location path: " << path << "\n";
+	auto itLocation = config.locations.find(path);
+	if (itLocation == config.locations.end())
+		return nullptr;
+	return &(*itLocation).second;
+}
+
+/*
+	Returns the Location struct for a specific path, if it exists, or nullptr, 
+	if it doesn't.
+*/
+const Location* ServerConfigData::getParentLocation(const Config& config, std::string path) {
+	if (path.size() > 1)
+			path.erase(path.begin() + path.find_last_of('/'), path.end());
+	std::cout << "Location path: " << path << "\n";
 	auto itLocation = config.locations.find(path);
 	if (itLocation == config.locations.end())
 		return nullptr;
