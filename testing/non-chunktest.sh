4198d423d14cd964bb6d6340834f6ff2f7e1e56d
Erno Michelsson <emichels@c1r3p1.hive.fi>
2025-05-09 10:29:22 +0300

    adds cgi extension check in location parser

--- testing/non-chunktest.sh
+++ testing/non-chunktest.sh
@@ -2,11 +2,13 @@
 
 echo -n "HulloWorld" > post_input.txt
 
-for port in {8080..8097}; do
-  echo "Testing port $port..."
-  curl -v -X POST http://localhost:$port/uploads/posttest_$port.txt \
-    -H "Content-Type: text/plain" \
-    --data-binary @post_input.txt
+for num in $(seq 1 5000); do
+  for port in {8080..8082}; do
+    echo "Testing port $port..."
+    curl -v -X POST http://localhost:$port/uploads/posttest_$port.txt \
+      -H "Content-Type: text/plain" \
+      --data-binary @post_input.txt
+  done
 done
 
 rm post_input.txt
